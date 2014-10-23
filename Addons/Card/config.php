343b6aae39607e850401cb72494e8bb5cdfb525f
HaraldLii <648267773@qq.com>
2014-10-23 11:57:35 +0800

    测试

--- Addons/Card/config.php
+++ Addons/Card/config.php
@@ -6,7 +6,7 @@ for ($i=1; $i<11;$i++){
 $bg[$i] = '自定义';
 return array (
 		'background' => array ( // 配置在表单中的键名 ,这个会是config[random]
-				'title' => '背景图:', // 表单的文字
+				'title' => '背景图:', // 表  单的文字
 				'type' => 'select', // 表单的类型：text、textarea、checkbox、radio、select等
 				'options'=>$bg,	
 				'value' => '1',
